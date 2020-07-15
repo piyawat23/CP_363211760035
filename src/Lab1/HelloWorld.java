48b435a541abda103a631519427dc169a1232f43
Piawat <Piyawat.p@rmutsvmail.com>
2020-07-15 14:56:17 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,12 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("hell World");
+        System.out.println("ปิยวัฒน์ เพิ่มพูล");
+        System.out.println("363211760035");
+        System.out.println("เทคโนโลยีการจัดการ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
+
+
 
 
 
