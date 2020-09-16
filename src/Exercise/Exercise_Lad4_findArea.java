f24fc163c7440cced67cf894c851f9b19d2d49f4
STP_COM08 <piyawat.p@rmutsvmail.com>
2020-09-16 13:44:19 +0700

    Chapter4_1-3_5_4

--- src/Exercise/Exercise_Lad4_findArea.java
+++ src/Exercise/Exercise_Lad4_findArea.java
@@ -1,7 +1,71 @@
 package Exercise;
 
+import java.util.Scanner;
+
+//พื้นที่สามเหลี่ยม =0.5 * w * h
+//พื้นที่วงกลม =pi * r * r * r
+//พื้นทรงกรวย =1/3 * pi * r * r * h
 public class Exercise_Lad4_findArea {
     public static void main(String[] args) {
+        // user select list menus 1-4
+        Scanner sc = new Scanner(System.in);
+        int select;
+        boolean b =false;
+        do {
+            System.out.println("Please, select menu below: ");
+            System.out.println("1. คำนวนพื้นที่สามเหลี่ยม");
+            System.out.println("2. คำนวนพื้นที่วงกลม");
+            System.out.println("3. คำนวนพื้นที่ทรงกรวย");
+            System.out.println("4. ออกจากโปรแกรม");
+            System.out.println(" เลือก ");
+            select = sc.nextInt();
+
+            switch (select){
+                case 1:
+                    findTiangleArea();
+                case 2 :
+                    findCircleArea();
+                case 3:
+                    findConeArea();
+                case 4:
+                    System.exit(1);
+                    break;
+                default:
+                    b = true;
+
+            }
+        } while (b);
+
+    }
+
+    private static void findConeArea() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("รัศมีของทรงกรวย: ");
+        double r = sc.nextDouble();
+        System.out.print("ความสูงของทรงกรวย: ");
+        double h = sc.nextDouble();
+        double area = (1.0/3.0) *3.141 * r * r * h ;
+        System.out.println("พื้นที่ทรงกรวย  ="+area);
+
+
+    }
+
+    private static void findCircleArea() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("รัศมีวงกลม:");
+        double r = sc.nextDouble();
+        double area = 3.141 * r * r * r ;
+        System.out.println("พื้นที่วงกลม  ="+area);
+    }
+
+    private static void findTiangleArea() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ความยาวฐานสามเหลี่ยม: ");
+        double b = sc.nextDouble();
+        System.out.print("ความสูงสามเหลี่ยม: ");
+        double h = sc.nextDouble();
+        double area = 0.5 * b * h;
+        System.out.println("พื้นที่สามเหลี่ยม = "+ area);
 
     }
 }
